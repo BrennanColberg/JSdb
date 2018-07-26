420cb93a0c3828ad16f23c9d58d325a9bcc10614
Brennan Colberg <brennan.colberg@gmail.com>
2018-07-26 14:02:27 -0700

    added optional textContent declaration in ce(tag)
    if adding a string after tag in ce(tag), it'll be incorporated as that element's textContent before returning

--- abbr.js
+++ abbr.js
@@ -1,7 +1,7 @@
 function $(id){return document.getElementById(id);}
 function qs(selector){return document.querySelector(selector);}
 function qsa(selector){return document.querySelectorAll(selector);}
-function ce(tag){return document.createElement(tag);}
+function ce(tag,str){let e=document.createElement(tag);if(str)e.textContent=str;return e;}
 function ctn(str){return document.createTextNode(str);}
 function wrap(num,min,max){let r=max-min+1;while(num>max){num-=r;}while(num<min){num+=r;}return num;}
 function clamp(num,min,max){return Math.min(Math.max(num,min),max);}
