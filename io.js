79e7f7a5f765a8ab7e972f379f03a85292fd7705
Brennan Colberg <brennan.colberg@gmail.com>
2018-07-26 11:00:46 -0700

    io.js implementation
    attempting to store JSON files easily in page cookies; still untested, just coded

--- io.js
+++ io.js
@@ -1,11 +1,27 @@
-function save(data, path) {
-	
+const MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+function saveData(data, days) {
+	let json = JSON.stringify(data);
+	let date = new Date();
+	date.setTime(date.getTime() + days * MS_PER_DAY);
+	let dataString = "data=" + json + "; ";
+	let expirationString = "expires=" + date.toUTCString(); + "; ";
+	document.cookie = dataString + expirationString + "path=/;";
 }
 
-function load(path) {
-	
+function loadData() {
+	let cookie = document.cookie;
+	let vars = cookie.split(';');
+	for (let i = 0; i < vars.length; i++) {
+		let parts = vars[i].split('=');
+		if (parts[0].trim === "data") {
+			let json = part[1].trim;
+			let data = JSON.parse(json);
+			return data;
+		}
+	}
 }
 
-function erase(path) {
-	
+function eraseData() {
+	saveData("", -1);
 }
