1f5ba1a1206c0a74e01086e447d9fff9636312ca
Brennan Colberg <brennan.colberg@gmail.com>
2018-07-26 12:40:21 -0700

    Minimized abbr.js & added min/max functions
    - minimized all code since it's simple and needs to be fast
    - added wrap, clamp, within

--- abbr.js
+++ abbr.js
@@ -1,4 +1,7 @@
-function $(id) { return document.getElementById(id); }
-function qs(selector) { return document.querySelector(selector); }
-function qsa(selector) { return document.querySelectorAll(selector); }
-function ce(tag) { return document.createElement(tag); }
+function $(id){return document.getElementById(id);}
+function qs(selector){return document.querySelector(selector);}
+function qsa(selector){return document.querySelectorAll(selector);}
+function ce(tag){return document.createElement(tag);}
+function wrap(num,min,max){let r=max-min+1;while(num>max){num-=r;}while(num<min){num+=r;}return num;}
+function clamp(num,min,max){return Math.min(Math.max(num,min),max);}
+function within(num,min,max){return num>=min&&num<=max;}
