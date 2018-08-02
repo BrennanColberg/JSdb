b7e2a140e93c647eb7dbec3b9c8da3b9754e0d08
Brennan Colberg <brennan.colberg@gmail.com>
2018-08-01 22:22:59 -0700

    Cookie manager!
    new js/cookie.js file
    - has saveCookie, loadCookie, eraseCookie
    - literally copied from io.js but that is more specified for axis-allies (perhaps this is not the best thing to use for various projects...?)

---
+++ js/cookie.js
@@ -0,0 +1,25 @@
+const MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+function saveCookie(title, data, path = "/", days = 30) {
+	let date = new Date();
+	date.setTime(date.getTime() + (days * MS_PER_DAY));
+	let dataString = "data=" + data + ";";
+	let expirationString = "expires=" + date.toUTCString() + ";";
+	let pathString = title.trim() + "=" + path;
+	document.cookie = dataString + expirationString + pathString;
+}
+
+function loadCookie(title, path = "/") {
+	let cookie = decodeURIComponent(document.cookie);
+	let vars = cookie.split(';');
+	for (let i = 0; i < vars.length; i++) {
+		let parts = vars[i].split('=');
+		if (parts[0] === title.trim()) {
+			return parts[1];
+		}
+	}
+}
+
+function eraseCookie(title, path = "/") {
+	saveCookie(title, "", path, -1);
+}
