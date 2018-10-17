a2c507e9a5df747377ce6f21f692352378c0fb57
Brennan Colberg <brennan.colberg@gmail.com>
2018-10-17 09:32:34 -0700

    Added & tested DOM shortcut methods including advanced ce(...)

--- testing/index.js
+++ testing/index.js
@@ -4,12 +4,14 @@
 	window.addEventListener("load", function () {
 
 		ajaxGET("return_args.php", function (text) {
-			$("output").textContent = text;
+			$("#output").textContent = text;
 		}, {
 			"test": 1,
 			"why": "no",
 			"foo": "bar"
 		});
+
+		$("body").appendChild(ce("ul", "test", ce("li", undefined, "hi this is the first method"), ce("li", undefined, "wow this is cool", ce("marquee", undefined, "this is deprecated!"))));
 
 	});
 
--- toolbox.js
+++ toolbox.js
@@ -3,7 +3,7 @@
 /**
  * Helper function used by AJAX methods; no useful normal function.
  * 
- * @param {Promise} r a promise to check the status of
+ * @param {object} r a promise to check the status of
  */
 function checkStatus(r) {
 	if (r.status >= 200 && r.status < 300) {
@@ -74,6 +74,49 @@ function ajaxPOST(url, onSuccess, arguments) {
 
 
 /***** Standard DOM Shortcuts *****/
+
+/**
+ * General shorthand selector function.
+ * 
+ * @param {string} selector CSS attribute to search with
+ * @param {object} parent an optional DOM element to base the search on
+ * @return {object} the selected DOM element
+ */
+function $(selector, parent) {
+	return (parent ? parent : document).querySelector(selector);
+}
+
+/**
+ * DOM element creation shortcut.
+ * 
+ * @param {string} tag  type of HTML element to create
+ * @param {string} className class of new element (optional)
+ * @param {string || object} content things to put inside (text or other DOMs)
+ * @return {object} generated DOM element
+ */
+function ce(tag, className, content) {
+	let element = document.createElement(tag);
+	if (className) element.className = className;
+	for (let i = 2; i < arguments.length; i++) {
+		let arg = arguments[i];
+		if (typeof arg === "string") {
+			element.textContent += arg;
+		} else if (isElement(arg)) {
+			element.appendChild(arg);
+		}
+	}
+	return element;
+}
+
+/**
+ * Simple test to determine whether an object is a DOM element. Does not work in IE7.
+ * 
+ * @param {object} object to test 
+ * @return {boolean} whether or not it's an element
+ */
+function isElement(element) {
+	return element instanceof Element || element instanceof HTMLDocument;
+}
 
 
 /***** cookie methods *****/
